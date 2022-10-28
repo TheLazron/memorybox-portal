0b351558433fd378ef1517aea6e52d2abfac03c8
Th0masCat <catsaredogstoo@gmail.com>
2022-10-28 12:51:59 +0530

    tatti mootui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import { TextField } from '@mui/material'
 import Login from './pages/Login'
+import VideoBg from './assets/video.mp4'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-gray-100 text-gray-800 px-4 py-6 h-screen flex justify-between m-5'>
-      <Login />
+    <div>
+    <video src={ VideoBg } autoPlay loop muted className='flex w-screen h-screen object-cover absolute'/>
+    <div className='flex relative p-40 gap-20'>
+        <Login/>
+    </div>
+
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,15 +23,19 @@ const theme = createTheme({
 const Login = ()=>{
     return(
         <ThemeProvider theme={theme}>
-        
-        <div className="m-auto text-white text-xl h-fit flex justify-center">Hello</div>
-        
-        <div className="m-auto p-20 bg-white rounded flex flex-col">
+      
+        <div className="m-auto text-white text-xl flex justify-center bg-black p-10 rounded-xl">Hello</div>
+        <div className="p-20 bg-white rounded-xl flex flex-col w-1/3">
             <h1 className=" text-peach text-xl w-fit">Login</h1>
             <TextField margin="normal" label="Email Address" size="small"/>
             <TextField label="Password" size="small"/>
             <br/>
-            <Button variant="contained" margin="normal" size="small" className="w-1/3">Text</Button>
+            <Button variant="contained" margin="normal" size="small" className="w-20" style={
+            {
+                padding: '5px',
+                color: 'white',
+            }
+            }>Sign In</Button>
         </div>
  
         </ThemeProvider>
